e18d54051e528116e631d8277d1a61c922707e2a
Kdavtyan21 <karendavtyan21@mail.com>
2019-10-29 15:58:10 -0400

    added schedule form edit and create

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Button from "components/Button"
+import InterviewerList from "components/InterviewerList"
+
+
+export default function Form (props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(null);
+  function reset() {
+    setName("")
+    setInterviewer(null)
+  } 
+  return (
+    <main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+    <form autoComplete="off"
+      onSubmit= {event => event.preventDefault()}
+    >
+      <input
+        className="appointment__create-input text--semi-bold"
+        name="name"
+        type="text"
+        placeholder="Enter Student Name"
+        value= {name}
+        onChange= {event => {
+          setName(event.target.value);
+        }}
+        /*
+          This must be a controlled component
+        */
+      />
+    </form>
+    <InterviewerList interviewers={props.interviewers} value={interviewer} interviewer={interviewer} setInterviewer={setInterviewer} />
+  </section>
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button onClick={() => props.onCancel(reset())} danger>Cancel</Button>
+      <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+    </section>
+  </section>
+</main>
+  )
+}
