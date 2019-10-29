04e36887bed37cf37bc69355cccacc1d943141e1
Kdavtyan21 <karendavtyan21@mail.com>
2019-10-28 20:25:32 -0400

    added more stories for appointment'

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,10 @@
 import React from "react"
 import "components/Appointment/styles.scss";
 import { storiesOf } from "@storybook/react";
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
+import { action } from "@storybook/addon-actions/dist/preview";
+
 
 export default function Appointment (props) {
   return (
@@ -13,4 +17,6 @@ storiesOf("Appointment", module)
   backgrounds: [{ name: "white", value: "#fff", default: true }]
 })
 .add("Appointment", () => <Appointment />)
-.add("Appointment with Time", () => <Appointment time="12pm" />);
+.add("Appointment with Time", () => <Appointment time="12pm" />)
+.add("Header", () => <Header time="12pm" />)
+.add("Empty", () => <Empty onAdd={action("onAdd")} />)
